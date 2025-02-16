5cbafa40f73d7024fff19311b063233f594e2191
EpphremM <mandefroephrem48@gmail.com>
2025-02-15 22:57:51 -1000

    setuping socket io fro notification chat and game

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -8,23 +8,23 @@ export const useSocket = () => {
     const [messages, setMessages] = useState<string[]>([]);
 
     useEffect(() => {
-        // Initialize the socket connection only once
+
         const socketInstance = io(SOCKET_SERVER_URL, {
-            transports: ['websocket'], // Ensure using WebSocket transport
+            transports: ['websocket'],
         });
 
         setSocket(socketInstance);
 
-        // Listen for 'sendMessage' event and update messages
+
         socketInstance.on("sendMessage", (msg: string) => {
             setMessages((prev) => [...prev, msg]);
         });
 
-        // Cleanup on unmount
+
         return () => {
             socketInstance.disconnect();
         };
-    }, []); // Empty dependency array to ensure this runs only once when component mounts
+    }, []);
 
     const sendMessage = (message: string) => {
         if (socket) socket.emit("sendMessage", message);
